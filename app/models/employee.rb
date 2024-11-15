e98eb628e14256a336fe2b177048590dab938edf
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-11-15 19:56:38 +0000

    added employee-user model controller and views with devise

---
+++ app/models/employee.rb
@@ -0,0 +1,35 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
+#  memberships_count      :integer
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  responsibilities_count :integer
+#  title                  :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  organization_id        :integer
+#
+# Indexes
+#
+#  index_employees_on_email                 (email) UNIQUE
+#  index_employees_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class Employee < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  belongs_to :organization, required: true, class_name: "Organization", foreign_key: "organization_id", counter_cache: true
+  #has_many  :responsibilities, class_name: "Responsibility", foreign_key: "employee_id", dependent: :nullify
+  #has_many  :memberships, class_name: "Member", foreign_key: "employee_id", dependent: :nullify
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,45 @@
 Rails.application.routes.draw do
+  # Routes for the Responsibility resource:
+
+  # CREATE
+  post("/insert_responsibility", { :controller => "responsibilities", :action => "create" })
+          
+  # READ
+  get("/responsibilities", { :controller => "responsibilities", :action => "index" })
+  
+  get("/responsibilities/:path_id", { :controller => "responsibilities", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_responsibility/:path_id", { :controller => "responsibilities", :action => "update" })
+  
+  # DELETE
+  get("/delete_responsibility/:path_id", { :controller => "responsibilities", :action => "destroy" })
+
+  #------------------------------
+
+  devise_for :employees
+
+  root "home#index"
+
+  # Routes for the Organization resource:
+
+  # CREATE
+  post("/insert_organization", { :controller => "organizations", :action => "create" })
+          
+  # READ
+  get("/organizations", { :controller => "organizations", :action => "index" })
+  
+  get("/organizations/:path_id", { :controller => "organizations", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_organization/:path_id", { :controller => "organizations", :action => "update" })
+  
+  # DELETE
+  get("/delete_organization/:path_id", { :controller => "organizations", :action => "destroy" })
+
+  #------------------------------
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
