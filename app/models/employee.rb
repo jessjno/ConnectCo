e7c1e5f210dabc6537669699086677a2c9617f6d
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-11-19 01:19:36 +0000

    updated some associations and implemented the organization_tree

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -28,6 +28,6 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  belongs_to :organization
   # TODO: associations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,54 +2,45 @@ Rails.application.routes.draw do
   devise_for :employees
   resources :employee, only: [:show, :edit, :update]
   root "organizations#index"
-  # Routes for the Member resource:
 
+  resources :organizations do
+    member do
+      get :employees # Adds a route for `/organizations/:id/employees`
+    end
+  end
+  # Routes for the Member resource:
   # CREATE
   post("/insert_member", { :controller => "members", :action => "create" })
-          
   # READ
   get("/members", { :controller => "members", :action => "index" })
-  
   get("/members/:path_id", { :controller => "members", :action => "show" })
-  
   # UPDATE
-  
   post("/modify_member/:path_id", { :controller => "members", :action => "update" })
-  
   # DELETE
   get("/delete_member/:path_id", { :controller => "members", :action => "destroy" })
 
   #------------------------------
 
   # Routes for the Responsibility resource:
-  
   # CREATE
   post("/insert_responsibility", { :controller => "responsibilities", :action => "create" })
-          
   # READ
   get("/responsibilities", { :controller => "responsibilities", :action => "index" })
   get("/responsibilities/:path_id", { :controller => "responsibilities", :action => "show" })
-  
   # UPDATE
   post("/modify_responsibility/:path_id", { :controller => "responsibilities", :action => "update" })
-  
   # DELETE
   get("/delete_responsibility/:path_id", { :controller => "responsibilities", :action => "destroy" })
-
   #------------------------------
-
 
   # Routes for the Organization resource:
   # CREATE
   post("/insert_organization", { :controller => "organizations", :action => "create" })
-          
   # READ
   get("/organizations", { :controller => "organizations", :action => "index" })
   get("/organizations/:path_id", { :controller => "organizations", :action => "show" })
-  
   # UPDATE
   post("/modify_organization/:path_id", { :controller => "organizations", :action => "update" })
-  
   # DELETE
   get("/delete_organization/:path_id", { :controller => "organizations", :action => "destroy" })
 end
