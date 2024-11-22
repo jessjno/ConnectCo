c77d4e10c97abf2a090a012a22d612c4246f6a06
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-11-22 01:32:43 +0000

    added the authenticate user back to set up sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # todo: uncomment later
-  # before_action :authenticate_employee!
+  before_action :authenticate_employee!
   skip_forgery_protection
 
   helper_method :current_employee
