d614f6a82cb1937f01cb09597a59ec570d121f3e
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-11-27 18:17:26 +0000

    refactored routes they were getting too confusing

--- config/routes.rb
+++ config/routes.rb
@@ -3,60 +3,28 @@ Rails.application.routes.draw do
   as :employee do
     get 'employees/sign_up', to: redirect('/'), as: :new_employee_registration
   end
-  resources :employees, only: [:show, :index, :edit, :update, :new, :create, :destroy]
+
   root "organizations#index"
 
-  resources :organizations, only: %i[index show new create edit update destroy]
   resources :organizations do
-    resources :employees, only: [:index, :show, :create, :edit, :update, :destroy]
-  end
-
-  resources :organizations do
+    resources :employees, only: [:index, :create, :edit, :update, :destroy]
     collection do
       post :upload_csv
     end
   end
 
   resources :employees do
+    resources :responsibilities, only: [:create, :edit, :update, :destroy]
     collection do
       post :upload_csv
     end
-  end
-  
-
-  resources :employees, only: [:show] do
     member do
-      get 'edit_responsibility/:id', to: 'employees#edit_responsibility', as: 'edit_responsibility'
-      patch 'update_responsibility/:id', to: 'employees#update_responsibility', as: 'update_responsibility'
+      get :edit_organization
+      patch :update_organization
     end
   end
 
-  get 'search_results', to: 'search#results'
-
   resources :responsibilities, only: [:create, :edit, :update, :destroy]
 
-  #------------------------------
-
-  # Routes for the Responsibility resource:
-  # CREATE
-  post("/insert_responsibility", { :controller => "responsibilities", :action => "create" })
-  # READ
-  get("/responsibilities", { :controller => "responsibilities", :action => "index" })
-  get("/responsibilities/:path_id", { :controller => "responsibilities", :action => "show" })
-  # UPDATE
-  post("/modify_responsibility/:path_id", { :controller => "responsibilities", :action => "update" })
-  # DELETE
-  get("/delete_responsibility/:path_id", { :controller => "responsibilities", :action => "destroy" })
-  #------------------------------
-
-  # Routes for the Organization resource:
-  # CREATE
-  post("/insert_organization", { :controller => "organizations", :action => "create" })
-  # READ
-  get("/organizations", { :controller => "organizations", :action => "index" })
-  get("/organizations/:id", { :controller => "organizations", :action => "show" })
-  # UPDATE
-  post("/modify_organization/:path_id", { :controller => "organizations", :action => "update" })
-  # DELETE
-  get("/delete_organization/:path_id", { :controller => "organizations", :action => "destroy" })
+  get 'search_results', to: 'search#results'
 end
