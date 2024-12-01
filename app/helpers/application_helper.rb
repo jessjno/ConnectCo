729390eb97dc6919c8d2782333a7d3408c3ab901
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-12-01 02:14:37 +0000

    add truncate to organization descriptions i

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def truncate_description(description, word_limit, view_more_path)
+    return "No description available." if description.blank?
+
+    words = description.split
+    if words.size > word_limit
+      truncated = words[0...word_limit].join(" ") + "..."
+      truncated + " " 
+    else
+      description
+    end
+  end
 end
