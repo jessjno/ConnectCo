8578d4bd5780187bc441f865b65103fce5978590
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-12-03 23:30:41 +0000

    refactored application.html.erb and fixed the @hotwired/turbo-rails error

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,8 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "controllers", to: "controllers/index.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.0.8
+pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin "jquery" # @3.7.1
